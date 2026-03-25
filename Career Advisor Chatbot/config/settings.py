7b90e541721993b0bcb29452bffda0c4daa00ae3
Akhiranand-Boddani <akhiranandboddani.edu@gmail.com>
2026-03-25 15:35:08 +0530

    chore: move project to subfolder for collection repository

---
+++ Career Advisor Chatbot/config/settings.py
@@ -0,0 +1,3 @@
+APP_NAME = "Career Advisor AI (Mistral)"
+MAX_MEMORY_TURNS = 5
+MISTRAL_MODEL = "open-mistral-nemo"
--- config/settings.py
+++
@@ -1,3 +0,0 @@
-APP_NAME = "Career Advisor AI (Mistral)"
-MAX_MEMORY_TURNS = 5
-MISTRAL_MODEL = "open-mistral-nemo"
